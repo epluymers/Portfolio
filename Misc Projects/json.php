852a82a65fe19889690261036baf00bc5abc32ef
Elicia <elicia.pluymers@gmail.com>
2025-09-29 14:53:54 -0400

    Removing unnecessary files
    removed test data files and misc files not linked to specific projects

--- Misc Projects/json.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// Set up associative array
-$data = array('success'=> true,'message'=>'Success message: worked!');
-
-// JSON encode and send back to the server
-echo json_encode($data);
-
-?>
